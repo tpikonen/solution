655e1a38ab14355a15fdcf6905ca0bf8f545919f
Teemu Ikonen <tpikonen@gmail.com>
2010-07-15 17:13:32 +0200

    find_bad_pixels.py: Tweak median filter -based bad pixel detection.
    Detect bad pixels based on the ratio of deviation / filtered.
    Also mask pixels which deviate grossly on only one frame.

--- find_bad_pixels.py
+++ find_bad_pixels.py
@@ -88,6 +88,7 @@ def pilatus2m_gapmask(chipgaps=False, chipedges=2):
 
 bool_add = ma.mask_or
 
+
 def bool_sub(A, B):
     """Array logical operation 'A - B', equal to ( A and (not B) ).
     Truth table:
@@ -100,25 +101,36 @@ def bool_sub(A, B):
     """
     return np.logical_and(A, np.logical_not(B))
 
-def find_bad_pixels(files, hot_threshold=np.inf, var_factor=5.0, chipgaps=False):
-    """Return a mask where pixels determined as dead or hot are masked"""
 
+def find_bad_pixels(files, hot_threshold=np.inf, var_factor=5.0, chipgaps=False):
+    """Return a mask where pixels determined as dead or hot are masked."""
+
+    # Reject pixel if the sum of this pixel in all frames is less than this
     dead_threshold = 1
-    medfilt_std_reject = 3.0
+    # Reject if ratio of pixel value to median filtered value exceeds this
+    # in all frames
+    medfilt_var_const = 3.0
+    # Reject if ratio of pixel value to median filtered value exceeds this
+    # even in a single frame
+    medfilt_var_gross = 6.0
+    # Curvature threshold above which deviates from median are not rejected
     laplace_threshold = 500.0
+
 
     # Laplace operator with diagonals, see
     # http://en.wikipedia.org/wiki/Discrete_Laplace_operator
     laplace = np.ones((3,3), dtype=np.float64)
     laplace[1,1] = -8.0
 
-
     f0 = read_cbf(files[0])
     s = f0.im.shape
     # Boolean arrays of various invalid pixels (logical_not of a mask!)
+    # Pixels above hot_threshold
     a_hots = ma.make_mask_none((s[0], s[1]))
-    # array for pixels consistently deviating from median filtered
+    # Pixels always deviating from median filtered
     a_medf = np.ones((s[0], s[1]), dtype=np.bool)
+    # Pixels grossly deviating from median filtered, even in a single frame
+    a_gross = np.zeros((s[0], s[1]), dtype=np.bool)
 
     Asum = np.zeros((s[0], s[1]), dtype=np.float64)
     Asumsq = np.zeros((s[0], s[1]), dtype=np.float64)
@@ -128,10 +140,15 @@ def find_bad_pixels(files, hot_threshold=np.inf, var_factor=5.0, chipgaps=False)
         Asumsq += tim**2.0
         a_hots = bool_add(a_hots, (tim > hot_threshold))
         mfilt = scipy.signal.medfilt2d(tim, kernel_size=3)
-        deviates = (np.abs(tim - mfilt) > medfilt_std_reject*np.sqrt(tim))
-        lapm = (np.abs(scipy.signal.convolve2d(laplace, mfilt, mode='same')) < laplace_threshold)
-        deviates = deviates * lapm
-        a_medf = a_medf * deviates
+        # Pixels where curvature is less than threshold
+        lapm = (np.abs(scipy.signal.convolve2d(mfilt, laplace, mode='same',
+            boundary='symm', old_behavior=0)) < laplace_threshold)
+        absdev = np.abs(tim - mfilt)
+        # Pixels deviating somewhat from median, not in curved regions
+        mrej = (medfilt_var_reject < (absdev/(mfilt+1))) * lapm
+        a_medf = a_medf * mrej
+        # Pixels deviating grossly from median filtered
+        a_gross = bool_add(a_gross, (medfilt_var_gross < (absdev/(mfilt+1))) * lapm)
 
     sA = Asum
     mA = Asum / float(len(files))
@@ -144,7 +161,7 @@ def find_bad_pixels(files, hot_threshold=np.inf, var_factor=5.0, chipgaps=False)
     a_randoms = False # Something strange with variance, disabling for now
     a_deads = bool_sub((sA < dead_threshold),  a_gaps)
     # total bads outside gaps
-    a_bads = reduce(bool_add, [a_consts, a_randoms, a_hots, a_deads, a_medf])
+    a_bads = reduce(bool_add, [a_consts, a_randoms, a_hots, a_deads, a_medf, a_gross])
 
     print("%d invalid pixels in module gaps" % (np.sum(a_gaps)))
     nconsts = np.sum(a_consts)
@@ -153,6 +170,7 @@ def find_bad_pixels(files, hot_threshold=np.inf, var_factor=5.0, chipgaps=False)
 #    print("Found %d pixels with variance larger than %f * mean" % (np.sum(m_randoms), var_factor))
     print("%d hot (count > %e) pixels" % (np.sum(a_hots), hot_threshold))
     print("%d pixels consistently deviating from median" % (np.sum(a_medf)))
+    print("%d pixels grossly deviating from median" % (np.sum(a_gross)))
     print("Total of %d bad pixels outside of gaps" % (np.sum(a_bads)))
     mask = np.ones((s[0], s[1]), dtype=np.bool)
     mask = bool_sub(mask, bool_add(a_bads, a_gaps))
