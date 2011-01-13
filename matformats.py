524c4c899636e6767afea6267e667f451624e6e0
Teemu Ikonen <tpikonen@gmail.com>
2011-01-13 19:48:39 +0100

    read_mat(): rename to read_matfile. read_matdict(): new function.
    read_matdict() reads variables from a mat-file to a dictionary.

--- matformats.py
+++ matformats.py
@@ -2,8 +2,30 @@ import IPython.ipapi, os.path
 import numpy as np
 from scipy.io.matlab import *
 
-def read_mat(fname, **kwargs):
+def read_matdict(fname, **kwargs):
+    """Read a struct from a mat-file to a dict in ipython interactive namespace.
+
+    Skips '__version__', '__globals__' and '__header__'.
+    See scipy.io.loadmat for keyword arguments.
+    """
+    d = loadmat(fname, struct_as_record=1, squeeze_me=1, chars_as_strings=1, **kwargs)
+    d.pop('__version__')
+    d.pop('__header__')
+    d.pop('__globals__')
+    api = IPython.ipapi.get()
+    for varname in d.keys():
+        rec = d[varname]
+        dout = {}
+        for kk in rec.dtype.fields.keys():
+            dout[kk] = rec[kk][()]
+        api.to_user_ns({varname : dout})
+        print(varname)
+
+
+def read_matfile(fname, **kwargs):
     """Read variables from a mat-file to ipython interactive namespace.
+
+    Does not do squeezing.
 
     Skips '__version__', '__globals__' and '__header__'.
     See scipy.io.loadmat for keyword arguments.
@@ -14,6 +36,8 @@ def read_mat(fname, **kwargs):
     d.pop('__globals__')
     api = IPython.ipapi.get()
     api.to_user_ns(d)
+    for vname in d.keys():
+        print(vname)
 
 
 def write_mat(varname, fname=None, value=None, overwrite=False):
