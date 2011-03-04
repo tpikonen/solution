c78b2b0e152960ac90acd24d33c1a2e81185801a
Teemu Ikonen <tpikonen@gmail.com>
2011-03-04 18:37:19 +0100

    Add CLI for subtract.py.

--- subtract.py
+++ subtract.py
@@ -1,8 +1,21 @@
+import sys
 import numpy as np
+from optparse import OptionParser
 from scipy.io.matlab import savemat, loadmat
 from xformats.yamlformats import read_ydat, read_yaml
 from xformats.matformats import read_matclean
 from biosxs_reduce import read_experiment_conf, errsubtract
+
+description="""\
+Subtract backgrounds from stacks.
+"""
+
+usage="%prog scans.yaml"
+
+default_conf = "experiment_conf.yaml"
+default_modulus = 10
+default_outdir = "./stacks"
+default_indir = "./stacks"
 
 
 def get_buf(indir, scanno, posno):
@@ -47,4 +60,25 @@ def subtract_background(scanfile, conffile, indir, outdir):
                 subs[pos,rep,1:3,:] = subs[pos,rep,1:3,:] / conc
         outname = "subs%02d" % scanno
         savemat(outdir+'/'+outname + ".mat", {outname: subs}, do_compression=1)
+
+
+def main():
+    oprs = OptionParser(usage=usage, description=description)
+    oprs.add_option("-c", "--conffile",
+        action="store", type="string", dest="conf", default=default_conf,
+        help="Configuration file to use. Default is '%s'" % default_conf)
+    oprs.add_option("-o", "--outdir",
+        action="store", type="string", dest="outdir", default=default_outdir,
+        help="Output directory. Default is '%s'" % default_outdir)
+    oprs.add_option("-i", "--indir",
+        action="store", type="string", dest="indir", default=default_indir,
+        help="Output directory. Default is '%s'" % default_outdir)
+    (opts, args) = oprs.parse_args()
+    if len(args) < 1:
+        oprs.error("Scanfile argument required")
+    scanfile = args[0]
+    subtract_background(scanfile, opts.conf, opts.indir, opts.outdir)
+
+if __name__ == "__main__":
+    main()
 
