026c6aea327898d202fce094ce4ce08f22720cdc
Teemu Ikonen <tpikonen@gmail.com>
2011-04-13 16:01:02 +0200

    splot.py: Move file input code to xformats.utils.

--- splot.py
+++ splot.py
@@ -5,8 +5,7 @@ import numpy
 import numpy as np
 import scipy.interpolate as ip
 from optparse import OptionParser
-from xformats.atsasformats import read_dat
-from xformats.yamlformats import read_loopyaml
+from xformats.utils import read_sasdata
 from sxsplots import plot_iq
 
 description="Plot DAT files."
@@ -27,23 +26,9 @@ def main():
     if(len(args) < 1):
         oprs.error("Input file argument required")
 
-    Iunit = 'unknown'
-    qunit = 'unknown'
     tups = []
     for fname in args:
-        if fname.endswith(('.yaml', '.ydat', '.yfil')):
-            yd = read_loopyaml(fname)
-            if yd.has_key('Ierr'):
-                dat = np.zeros((len(yd['Ierr']), 3))
-                dat[:,2] = np.array(yd['Ierr'])
-            else:
-                dat = np.zeros((2,len(yd['q'])))
-            dat[:,0] = np.array(yd['q'])
-            dat[:,1] = np.array(yd['I'])
-            Iunit = yd.get('I~unit', Iunit)
-            qunit = yd.get('q~unit', qunit)
-        else:
-            dat = read_dat(fname).T
+        dat, qunit, Iunit = read_sasdata(fname)
         tups.append((dat, fname))
 
     fig = plt.figure()
