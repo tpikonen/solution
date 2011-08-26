dad2a0251952df2154cc6725d617eefc17d59501
Teemu Ikonen <tpikonen@gmail.com>
2011-08-26 15:37:06 +0200

    Add a Guinier plot function: plot_guinier().

--- sxsplots.py
+++ sxsplots.py
@@ -119,3 +119,48 @@ def plot_iq(ax, dat, err=1, smerr=0, **kwargs):
     if label:
         ax.legend()
     ax.hold(savehold)
+
+
+def plot_guinier(ax, dat, grange=None, Rg=None, I0=None, conc=np.nan, annotate=1, *kwargs):
+    """Guinier plot of `dat` to axis `ax`."""
+    def guinier(I0, Rg, q):
+        return I0*np.exp(-(1.0/3)*(Rg*q)**2)
+    inds = clean_indices(dat, np.ones_like(dat))
+    q = dat[0,:]
+    I = dat[1,:]
+    Ierr = dat[2,:]
+    if grange:
+        glen = grange[1] - grange[0]
+        pmin = max(0, grange[0] - glen/2)
+        pmax = min(len(inds), grange[1] + glen/3)
+        inds[0:pmin] = False
+        inds[pmax:-1] = False
+        inds[-1] = False
+    restxt = "c = %3.3f g/l, I(0) = %3.3f, Rg = %3.3f nm" % (conc, I0, Rg)
+    ax.errorbar(q[inds]**2, np.log10(I[inds]),\
+        (1.0/np.log(10.0))*np.abs(Ierr[inds]/I[inds]), fmt='.', label=restxt,
+        capsize=0)
+    qline = q[inds]
+    l0 = ax.plot(qline**2, np.log10(guinier(I0, Rg, qline)))[0]
+    lcolor = l0.properties()['color']
+    gstart = (q[grange[0]]**2, np.log10(guinier(I0, Rg, q[grange[0]])))
+    gend = (q[grange[1]]**2, np.log10(guinier(I0, Rg, q[grange[1]])))
+    plt.plot(gstart[0], gstart[1],
+        '|', markersize=100, c=lcolor)
+    plt.plot(gend[0], gend[1],
+        '|', markersize=100, c=lcolor)
+
+    if annotate:
+        plt.annotate("q*Rg = %0.3g" % (Rg*q[grange[0]]), gstart,
+            textcoords='offset points', xytext=(0, 60),
+            arrowprops=dict(arrowstyle="->",
+                connectionstyle="arc,angleA=180,armA=60,angleB=100,armB=20,rad=7"))
+        plt.annotate("q*Rg = %0.3g" % (Rg*q[grange[1]]), gend,
+            textcoords='offset points', xytext=(0, 60),
+            arrowprops=dict(arrowstyle="->",
+                connectionstyle="arc,angleA=180,armA=60,angleB=100,armB=25,rad=7"))
+
+    plt.legend()
+    plt.xlabel("q**2 / (1/nm**2)")
+    plt.ylabel("log10(I) / arb.")
+    plt.show()
